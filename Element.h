7ca3a2d41ee3b4064b7e3d2bdfb7c6524f70393f
bugemdai <37519790+bugemdai@users.noreply.github.com>
2025-03-13 11:03:41 +0300

    Update Element.h

--- Element.h
+++ Element.h
@@ -1,116 +1,76 @@
-﻿#ifndef _ELEMENT_H
+#ifndef _ELEMENT_H
 #define _ELEMENT_H
 
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
 #include <gl/freeglut.h>
 #include <gl/glut.h>
+#include <array>
+#include <algorithm>
 
 struct Element
 {
-	unsigned int color[6];
+	static constexpr int NUM_FACES = 6;
+	std::array<unsigned int, NUM_FACES> color;
 	double size;
+	unsigned char _color[4];
 
-	Element(){
-		memset(color, 0, sizeof(color));
-		size = 0.0;
+	Element() : size(0.0) {
+		std::fill(color.begin(), color.end(), 0);
 	}
 
-	void rotateZ(){
-		unsigned int tmp = color[5];
-		color[5] = color[3];
-		color[3] = color[4];
-		color[4] = color[2];
-		color[2] = tmp;
+	void rotateZ() {
+		std::rotate(color.rbegin(), color.rbegin() + 1, color.rend());
 	}
 
-	void rotateY(){
-		unsigned int tmp = color[2];
-		color[2] = color[1];
-		color[1] = color[3];
-		color[3] = color[0];
-		color[0] = tmp;
+	void rotateY() {
+		std::swap(color[0], color[2]);
+		std::swap(color[1], color[3]);
 	}
 
-	void rotateX(){
-		unsigned int tmp = color[0];
-		color[0] = color[4];
-		color[4] = color[1];
-		color[1] = color[5];
-		color[5] = tmp;
+	void rotateX() {
+		std::swap(color[0], color[4]);
+		std::swap(color[1], color[5]);
 	}
 
-	void setColor(int i, int color){
-		this->color[i] = color;
+	void setColor(int i, int colorValue) {
+		if (i >= 0 && i < NUM_FACES) {
+			color[i] = colorValue;
+		}
 	}
 
-	unsigned char* at(int i){
-		_color[0] = color[i] >> 16;
-		_color[1] = color[i] >> 8;
-		_color[2] = color[i];
+	unsigned char* at(int i) {
+		if (i >= 0 && i < NUM_FACES) {
+			_color[0] = (color[i] >> 16) & 0xFF;
+			_color[1] = (color[i] >> 8) & 0xFF;
+			_color[2] = color[i] & 0xFF;
+		}
 		return _color;
 	}
 
-	void draw(){
+	void draw() {
 		glPushMatrix();
 		glBegin(GL_QUADS);
 
-		glColor3ubv(at(0));
-		glNormal3f(0, 0, 1);
-		glVertex3f(size, size, size);
-		glVertex3f(0, size, size);
-		glVertex3f(0, 0, size);
-		glVertex3f(size, 0, size);
-
-		glColor3ubv(at(1));
-		glNormal3f(0, 0, -1);
-		glVertex3f(size, 0, 0);
-		glVertex3f(0, 0, 0);
-		glVertex3f(0, size, 0);
-		glVertex3f(size, size, 0);
-
-		glColor3ubv(at(2));
-		glNormal3f(0, -1, 0);
-		glVertex3f(size, 0, size);
-		glVertex3f(0, 0, size);
-		glVertex3f(0, 0, 0);
-		glVertex3f(size, 0, 0);
-
-		glColor3ubv(at(3));
-		glNormal3f(0, 1, 0);
-		glVertex3f(size, size, 0);
-		glVertex3f(0, size, 0);
-		glVertex3f(0, size, size);
-		glVertex3f(size, size, size);
-
-		glColor3ubv(at(4));
-		glNormal3f(-1, 0, 0);
-		glVertex3f(0, size, size);
-		glVertex3f(0, size, 0);
-		glVertex3f(0, 0, 0);
-		glVertex3f(0, 0, size);
-
-		glColor3ubv(at(5));
-		glNormal3f(1, 0, 0);
-		glVertex3f(size, size, 0);
-		glVertex3f(size, size, size);
-		glVertex3f(size, 0, size);
-		glVertex3f(size, 0, 0);
+		for (int i = 0; i < NUM_FACES; ++i) {
+			glColor3ubv(at(i));
+			glNormal3f(0, 0, 1);
+			glVertex3f(size, size, size);
+			glVertex3f(0, size, size);
+			glVertex3f(0, 0, size);
+			glVertex3f(size, 0, size);
+		}
 
 		glEnd();
 		glPopMatrix();
 	}
 
-	void draw(double x, double y, double z){
+	void draw(double x, double y, double z) {
 		glPushMatrix();
 		glTranslated(x, y, z);
 		draw();
 		glPopMatrix();
 	}
-
-private:
-	unsigned char _color[4];
 };
-
 
 #endif;
